20bd9e1f6584e3ed99b8e3c9f9a3b87e1dc12f92
2ndline <bob.murrell@gmail.com>
2021-01-01 20:22:18 -0600

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import * as L from "leaflet";
 import "leaflet.markercluster";
 import { HttpClient } from "@angular/common/http";
-import { GoogleSheetsDbService } from "ng-google-sheets-db";
-import { BehaviorSubject, Observable } from "rxjs";
+import { BehaviorSubject } from "rxjs";
 import {
   characterAttributesMapping,
   PrecinctVoterData
@@ -19,13 +18,15 @@ import { MapDataService } from "./services/map-data.service";
 export class AppComponent implements OnInit {
   map: L.Map;
   markers: L.Marker[] = [];
+  total: PrecinctVoterData;
+  public selectedColumn: string = "giarrussoPercent";
+  public columns: string[] = [];
   constructor(private http: HttpClient, private dataService: MapDataService) {}
   public precincts: Precinct[] = [];
   precintGeoJson: L.GeoJSON<any>;
 
-  svg =
+  private svg =
     '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 3h-4.18C14.4 1.84 13.3 1 12 1c-1.3 0-2.4.84-2.82 2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-7 0c.55 0 1 .45 1 1s-.45 1-1 1-1-.45-1-1 .45-1 1-1zm2 14H7v-2h7v2zm3-4H7v-2h10v2zm0-4H7V7h10v2z"/></svg>';
-  iconUrl = "data:image/svg+xml;base64," + btoa(this.svg);
   public DistrictAPrecincts: string[] = [
     "14-13A",
     "5-8",
@@ -108,7 +109,7 @@ export class AppComponent implements OnInit {
     "14-7",
     "14-6"
   ];
-  dataLoaded: BehaviorSubject<boolean> = new BehaviorSubject(false);
+  private dataLoaded: BehaviorSubject<boolean> = new BehaviorSubject(false);
   ngOnInit() {
     this.init();
   }
@@ -127,75 +128,77 @@ export class AppComponent implements OnInit {
           zoomControl: false,
           zoom: 12
         });
-        this.setPrecinctLayers("giarrussoPercent");
+        this.setPrecinctLayers();
       }
     });
     this.loadPrecinctData();
   }
 
-  setPrecinctLayers(columnForValue: string) {
+  public precinctUrl : string=
+    "https://opendata.arcgis.com/datasets/ca0f4261673541d798551f5cddc54bd6_0.geojson";
+
+  public sheetUrl = "1syXU60xTSCtHoePb7Yobh892SfdNTlYp1zuxyLPOtQg";
+
+  setPrecinctLayers() {
     let districtA = this.DistrictAPrecincts;
     let prs: Precinct[] = this.precincts;
     // Load geojson file
-    this.http
-      .get(
-        "https://opendata.arcgis.com/datasets/ca0f4261673541d798551f5cddc54bd6_0.geojson"
-      )
-      .subscribe((json: any) => {
-        let eachFunc = function(f, l) {
-          const precinctId = f.properties.PRECINCTID;
-          let pr: Precinct = prs[precinctId];
-          if (!pr) {
-            pr = { id: precinctId };
-          }
-          pr.feature = f;
-          pr.layer = l;
-          if (districtA.includes(precinctId)) {
-            //style layer & bind popup
-            pr.layer["options"].weight = 1;
-            let amount: number = pr.data[columnForValue];
-            let red = 255;
-            let green = 0;
-            if (amount >= 0.5) {
-              let diff = 1 - amount;
-              red = 510 * diff;
-              green = 255;
-            } else {
-              green = 510 * amount;
-              red = 255;
-            }
-            pr.layer["options"].fillColor =
-              "rgb(" + Math.round(red) + "," + Math.round(green) + ",0)";
-            pr.layer["options"].fillOpacity = 0.8;
-            pr.layer.bindPopup(
-              `<pre>${JSON.stringify(pr.data, null, 2)}</pre>`
-            );
+    this.http.get(this.precinctUrl).subscribe((json: any) => {
+      let eachFunc = function(f, l) {
+        const precinctId = f.properties.PRECINCTID;
+        let pr: Precinct = prs[precinctId];
+        if (!pr) {
+          pr = { id: precinctId };
+        }
+        pr.feature = f;
+        pr.layer = l;
+        if (districtA.includes(precinctId)) {
+          //style layer & bind popup
+          pr.layer["options"].weight = 1;
+          let amount: number = pr.data[this.selectedColumn];
+          let red = 255;
+          let green = 0;
+          if (amount >= 0.5) {
+            let diff = 1 - amount;
+            red = 510 * diff;
+            green = 255;
           } else {
-            pr.layer["options"].weight = 0;
+            green = 510 * amount;
+            red = 255;
           }
-        };
+          pr.layer["options"].fillColor =
+            "rgb(" + Math.round(red) + "," + Math.round(green) + ",0)";
+          pr.layer["options"].fillOpacity = 0.8;
+          pr.layer.bindPopup(`<pre>${JSON.stringify(pr.data, null, 2)}</pre>`);
+        } else {
+          pr.layer["options"].weight = 0;
+        }
+      };
 
-        this.precintGeoJson = L.geoJSON(json, {
-          onEachFeature: eachFunc
-        });
-        this.precintGeoJson.setStyle({
-          color: "black"
-        });
-        this.precincts = prs;
-        this.precintGeoJson.addTo(this.map);
+      this.precintGeoJson = L.geoJSON(json, {
+        onEachFeature: eachFunc
       });
+      this.precintGeoJson.setStyle({
+        color: "black"
+      });
+      this.precincts = prs;
+      this.precintGeoJson.addTo(this.map);
+    });
   }
 
   precinctVoterData: PrecinctVoterData[];
 
   loadPrecinctData() {
     this.dataService
-      .fetchDataFromSheet("1syXU60xTSCtHoePb7Yobh892SfdNTlYp1zuxyLPOtQg")
+      .fetchDataFromSheet(this.sheetUrl)
       .subscribe(results => {
         this.precinctVoterData = results;
         for (let i = 0; i < this.precinctVoterData.length; ++i) {
           let voterData = this.precinctVoterData[i];
           if (voterData.precinct == "Total") {
+            this.total = voterData;
+            this.columns = Object.keys(voterData);
+            this.selectedColumn = this.columns[0];
             continue;
           }
           //make sure precinct values have no leading 0s
@@ -213,12 +216,12 @@ export class AppComponent implements OnInit {
       });
   }
 
+  public valueSelected() {
+    this.setPrecinctLayers();
+  }
+
   /**
    * TODO:
-   * 1) Fix defect where some columns aren't loading (bernieLiz,registeredVoters )
-   * 2) Add nav header & filters form with the following: https://jsfiddle.net/xf4fwwme/44/
-   *    a) Spreadsheet URL to load data from
-   *    b) Which data element to color code (from the data columns)
    * 3) render precincts different if they aren't a part of the data set
    */
 
