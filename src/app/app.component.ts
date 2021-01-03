ac6f9171975645cdb51fd390ce5f3369fc4a5d45
2ndline <bob.murrell@gmail.com>
2021-01-02 20:42:15 -0600

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,7 @@ import * as L from "leaflet";
 import "leaflet.markercluster";
 import { HttpClient } from "@angular/common/http";
 import { BehaviorSubject } from "rxjs";
-import {
-  characterAttributesMapping,
-  PrecinctVoterData
-} from "./models/precinct-voter-data.model";
+import { PrecinctVoterData } from "./models/precinct-voter-data.model";
 import { Precinct } from "./models/precinct.model";
 import { MapDataService } from "./services/map-data.service";
 import { MapColumn } from "./models/mapColumn";
@@ -26,9 +23,6 @@ export class AppComponent implements OnInit {
   constructor(private http: HttpClient, private dataService: MapDataService) {}
   public precincts: Precinct[] = [];
   precintGeoJson: L.GeoJSON<any>;
-
-  private svg =
-    '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 3h-4.18C14.4 1.84 13.3 1 12 1c-1.3 0-2.4.84-2.82 2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-7 0c.55 0 1 .45 1 1s-.45 1-1 1-1-.45-1-1 .45-1 1-1zm2 14H7v-2h7v2zm3-4H7v-2h10v2zm0-4H7V7h10v2z"/></svg>';
 
   private dataLoaded: BehaviorSubject<boolean> = new BehaviorSubject(false);
   ngOnInit() {
@@ -123,9 +117,6 @@ export class AppComponent implements OnInit {
   json: any;
 
   setPrecinctLayers() {
-    let selectedColumn = this.selectedColumn;
-    let totalColumn = this.total;
-    let prs: Precinct[] = this.precincts;
     // Load geojson file
     this.http.get(this.precinctUrl).subscribe((json: any) => {
       this.json = json;
